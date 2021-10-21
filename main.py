6c1a74744b52f92acaf31dd7c210ad3d7a3c9d10
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2021-10-21 10:51:44 +0200

    nic

--- main.py
+++ main.py
@@ -4,102 +4,101 @@ def on_button_pressed_a():
     basic.show_icon(IconNames.YES)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_logo_up():
+def on_gesture_logo_up():
     global pocet_sten
     if pocet_sten == 6:
         pocet_sten = 10
     else:
         pocet_sten = 6
     basic.show_number(pocet_sten)
-input.on_gesture(Gesture.LOGO_UP, on_logo_up)
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
 
 def on_gesture_shake():
     global povoleno
-    global pocet_sten
     if povoleno:
         cislo = randint(1, pocet_sten)
         if cislo == 1:
             basic.show_leds("""
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
             """)
         if cislo == 2:
             basic.show_leds("""
-            . . . . .
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
+                . . . . .
+                . . . . .
+                . # . # .
+                . . . . .
+                . . . . .
             """)
         if cislo == 3:
             basic.show_leds("""
-            # . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . #
+                # . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . #
             """)
         if cislo == 4:
             basic.show_leds("""
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
+                . . . . .
+                . # . # .
+                . . . . .
+                . # . # .
+                . . . . .
             """)
         if cislo == 5:
             basic.show_leds("""
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
             """)
         if cislo == 6:
             basic.show_leds("""
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
             """)
         if cislo == 7:
             basic.show_leds("""
-            # . . . #
-            . . . . .
-            # . # . #
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                # . # . #
+                . . . . .
+                # . . . #
             """)
         if cislo == 8:
             basic.show_leds("""
-            # . # . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . # . #
+                # . # . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . # . #
             """)
         if cislo == 9:
             basic.show_leds("""
-            # . # . #
-            . . . . .
-            # . # . #
-            . . . . .
-            # . # . #
+                # . # . #
+                . . . . .
+                # . # . #
+                . . . . .
+                # . # . #
             """)
         if cislo == 10:
             basic.show_leds("""
-            # . . . #
-            . # . # .
-            # . . . #
-            . # . # .
-            # . . . #
+                # . . . #
+                . # . # .
+                # . . . #
+                . # . # .
+                # . . . #
             """)
-        for a in range(cislo):
-            music.play_tone(Note.E, music.beat(4))
+        for index in range(cislo):
+            music.play_tone(330, music.beat(4))
             music.rest(music.beat(2))
         povoleno = False
         basic.pause(1000)
--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     povoleno = true
     basic.showIcon(IconNames.Yes)
 })
-input.onGesture(Gesture.LogoUp, function on_logo_up() {
+input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
     
     if (pocet_sten == 6) {
         pocet_sten = 10
@@ -16,111 +16,110 @@ input.onGesture(Gesture.LogoUp, function on_logo_up() {
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     let cislo: number;
     
-    
     if (povoleno) {
         cislo = randint(1, pocet_sten)
         if (cislo == 1) {
             basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
             `)
         }
         
         if (cislo == 2) {
             basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
+                . . . . .
+                . . . . .
+                . # . # .
+                . . . . .
+                . . . . .
             `)
         }
         
         if (cislo == 3) {
             basic.showLeds(`
-            # . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . #
+                # . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . #
             `)
         }
         
         if (cislo == 4) {
             basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
+                . . . . .
+                . # . # .
+                . . . . .
+                . # . # .
+                . . . . .
             `)
         }
         
         if (cislo == 5) {
             basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
             `)
         }
         
         if (cislo == 6) {
             basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
             `)
         }
         
         if (cislo == 7) {
             basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . # . #
-            . . . . .
-            # . . . #
+                # . . . #
+                . . . . .
+                # . # . #
+                . . . . .
+                # . . . #
             `)
         }
         
         if (cislo == 8) {
             basic.showLeds(`
-            # . # . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . # . #
+                # . # . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . # . #
             `)
         }
         
         if (cislo == 9) {
             basic.showLeds(`
-            # . # . #
-            . . . . .
-            # . # . #
-            . . . . .
-            # . # . #
+                # . # . #
+                . . . . .
+                # . # . #
+                . . . . .
+                # . # . #
             `)
         }
         
         if (cislo == 10) {
             basic.showLeds(`
-            # . . . #
-            . # . # .
-            # . . . #
-            . # . # .
-            # . . . #
+                # . . . #
+                . # . # .
+                # . . . #
+                . # . # .
+                # . . . #
             `)
         }
         
-        for (let a = 0; a < cislo; a++) {
-            music.playTone(Note.E, music.beat(4))
+        for (let index = 0; index < cislo; index++) {
+            music.playTone(330, music.beat(4))
             music.rest(music.beat(2))
         }
         povoleno = false
